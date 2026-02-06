a197c4ca790bfe50d8017d3f1c8dac86dc5ad660
Avadheshkumarchauhan <avadheaha@gmail.com>
2026-02-06 12:12:15 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config({quiet:true});
-import connectDB from './dbs/user.db.js'
-import app from'./app.js';
+import connectDB from './src/dbs/user.db.js'
+import app from'./src/app.js';
 const PORT = process.env.PORT ||5000;
 const HOSTNAME = process.env.HOSTNAME ;
 try {    
